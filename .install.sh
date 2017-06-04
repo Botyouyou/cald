e90d36c9058facb21a9da419dab45b5a5f90c09f
volkovasystems <richeve.bebedor@gmail.com>
2017-06-04 20:30:57 +0800

    Finished cald procedure.

---
+++ .install.sh
@@ -0,0 +1,22 @@
+yarn add \
+babel-runtime \
+protype \
+shft \
+wichevr \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+protype \
+shft \
+wichevr \
+zelf \
+--save \
+--force-latest
