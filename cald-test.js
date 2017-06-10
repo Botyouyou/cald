fea19d858e8b48e642873bdcf750c85e17b57364
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-10 14:20:43 +0800

    Updated server/client test.

--- cald-test.js
+++ cald-test.js
@@ -3,19 +3,23 @@ const assert = require( "assert" );
 const cald = require( "./cald.js" );
 
 assert.deepEqual( cald( function hello( value ){
-	return [ "hey", value, this ];
-}, { "hello": "world" }, "yeah" ), [ "hey", "yeah", { "hello": "world" } ], "should be deeply equal" );
+		return [ "hey", value, this ];
+	}, { "hello": "world" }, "yeah" ),
+	[ "hey", "yeah", { "hello": "world" } ], "should be deeply equal" );
 
 assert.deepEqual( cald( function hello( value, data ){
-	return [ "hey", value, data, this ];
-}, { "hello": "world" }, "yeah", 123 ), [ "hey", "yeah", 123, { "hello": "world" } ], "should be deeply equal" );
+		return [ "hey", value, data, this ];
+	}, { "hello": "world" }, "yeah", 123 ),
+	[ "hey", "yeah", 123, { "hello": "world" } ], "should be deeply equal" );
+
+assert.deepEqual( cald( function test( parameter ){
+		return Array.from( arguments );
+	}, "test", [ "hello", [ "world" ], "yeah" ] ),
+	[ [ "hello", [ "world" ], "yeah" ] ], "should be deeply equal" );
 
 assert.deepEqual( cald( function test( parameter ){
 	return Array.from( arguments );
-}, "test", [ "hello", [ "world" ], "yeah" ] ), [ [ "hello", [ "world" ], "yeah" ] ], "should be deeply equal" );
-
-assert.deepEqual( cald( function test( parameter ){
-	return Array.from( arguments );
-}, "test", [ "hello", "world", "yeah" ], "hey" ), [ [ "hello", "world", "yeah" ], "hey" ], "should be deeply equal" );
+	}, "test", [ "hello", "world", "yeah" ], "hey" ),
+	[ [ "hello", "world", "yeah" ], "hey" ], "should be deeply equal" );
 
 console.log( "ok" );
